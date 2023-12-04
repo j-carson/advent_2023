2598432384f65deded8b6a78cae373947fea960c
j-carson <j-carson@fastmail.com>
2023-12-03 21:35:30 -0800

    Day 4

---
+++ days/04/part1.py
@@ -0,0 +1,74 @@
+import sys
+from dataclasses import dataclass, field
+from pathlib import Path
+
+import pytest
+from icecream import ic
+
+# --> Puzzle solution
+
+
+@dataclass
+class Card:
+    id_: int
+    numbers: set[int] = field(default_factory=set)
+    winning_numbers: set[int] = field(default_factory=set)
+
+    @classmethod
+    def from_line(cls, line: str):
+        card_id, cards = line.split(":")
+
+        my_card, winning_card = cards.split("|")
+        id_ = int(card_id.split()[1])
+        numbers = {int(i) for i in my_card.split()}
+        winning_numbers = {int(i) for i in winning_card.split()}
+        card = cls(id_, numbers, winning_numbers)
+
+        ic(card.id_, card.score)
+
+        return card
+
+    @property
+    def score(self):
+        overlap = len(self.numbers.intersection(self.winning_numbers))
+        if overlap:
+            return 2 ** (overlap - 1)
+        return 0
+
+
+def solve(input_data):
+    cards = [Card.from_line(line) for line in input_data.splitlines()]
+    return sum(c.score for c in cards)
+
+
+# --> Test driven development helpers
+
+
+# Test any examples given in the problem
+
+sample = Path("input-sample.txt").read_text().strip()
+EXAMPLES = [(sample, 13)]
+
+
+@pytest.mark.parametrize("sample_data,sample_solution", EXAMPLES, ids=("sample",))
+def test_samples(sample_data, sample_solution) -> None:
+    assert solve(sample_data) == sample_solution
+
+
+# --> Setup and run
+
+if __name__ == "__main__":
+    #  Run the test examples with icecream debug-trace turned on
+    ic.enable()
+    ex = pytest.main([__file__, "--capture=tee-sys", "-v"])
+    if ex not in {pytest.ExitCode.OK, pytest.ExitCode.NO_TESTS_COLLECTED}:
+        print(f"tests FAILED ({ex})")
+        sys.exit(1)
+    else:
+        print("tests PASSED")
+
+    #  Actual input data generally has more iterations, turn off log
+    ic.disable()
+    my_input = Path("input.txt").read_text().strip()
+    result = solve(my_input)
+    print(result)
